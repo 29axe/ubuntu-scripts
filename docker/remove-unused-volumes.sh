6c87564cba4215a84a5dfd879b70834d3289580c
hud <authentication.dh@gmail.com>
2018-03-01 16:08:10 +0100

    fix qgis install script and add docker volumes cleanup scripts

---
+++ docker/remove-unused-volumes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker volume rm $(docker volume ls -qf dangling=true)
+docker volume ls -qf dangling=true | xargs -r docker volume rm
+
--- install-qgis.sh
+++ install-qgis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # add repository 
 sudo cp /etc/apt/sources.list /tmp/sources.list.bak
-echo -e "deb     https://qgis.org/debian $(lsb_release -cs) main\ndeb-src\n#https://qgis.org/debian $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list >/dev/null
+echo -e "deb     https://qgis.org/debian $(lsb_release -cs) main\n#deb-src     https://qgis.org/debian $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list >/dev/null
 
 # get qgis key
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key CAEB3DC3BDF7FB45
